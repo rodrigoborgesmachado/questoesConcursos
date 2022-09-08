80967b127c2d54381511f92518b0f0fde4318837
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2022-09-08 11:21:22 -0300

    Corrigindo maiúsculo e minúsculo

--- react/questoesconcurso/src/pages/Home/index.js
+++ react/questoesconcurso/src/pages/Home/index.js
@@ -11,8 +11,8 @@ function Home(){
             </h3>
             <br/>
             <div className='botoes'>
-                <Link className='botao' to={`/questoes/aleatoria`}>Iniciar com Questões Aleatórias</Link>
-                <Link className='botao' to={`/questoes/enem`}>Iniciar com Questões do Enem</Link>
+                <Link className='botao' to={`/questoes/aleatoria`}>Iniciar com questões aleatórias</Link>
+                <Link className='botao' to={`/questoes/enem`}>Iniciar com questões do ENEM</Link>
                 <Link className='botao' to={`/materias`}>Selecionar questões por matéria</Link>
                 <Link className='botao' to={`/bancas`}>Selecionar questões por bancas</Link>
                 <Link className='botao' to={`/provas`}>Selecionar questões por prova</Link>
