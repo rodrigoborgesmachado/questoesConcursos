934367f192714e4d2fda6c6515a2984af70a6aab
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-05-04 17:14:09 -0300

    Corrigindo página de erro.

--- react/questoesconcurso/src/pages/Erro/index.js
+++ react/questoesconcurso/src/pages/Erro/index.js
@@ -3,10 +3,12 @@ import './style.css';
 
 function Erro(){
     return(
-        <div className="not-found">
-            <h1>404</h1>
-            <h2>Página não encontrada</h2>
-            <Link to="/">Home</Link>
+        <div className='containerpage'>
+            <div className="not-found">
+                <h1>404</h1>
+                <h2>Página não encontrada</h2>
+                <Link to="/">Home</Link>
+            </div>
         </div>
     )
 }
