7c327b85218a4df141fce2a12b9038444946e500
viniciusxpb <58737153+viniciusxpb@users.noreply.github.com>
2023-08-09 12:24:28 -0300

    correção admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -9,74 +9,74 @@ import makeAnimated from 'react-select/animated';
 import Stack from '@mui/material/Stack';
 import Pagination from '@mui/material/Pagination';
 
-function Admin(){
+function Admin() {
     const navigate = useNavigate();
     const animatedComponents = makeAnimated();
-    const[loadding, setLoadding] = useState(false);
-    const[respostas, setRespostas] = useState([]);
-    const[usuarios, setUsuarios] = useState([]);
-    const[usuarioFiltro, setUsuarioFiltro] = useState([]);
+    const [loadding, setLoadding] = useState(false);
+    const [respostas, setRespostas] = useState([]);
+    const [usuarios, setUsuarios] = useState([]);
+    const [usuarioFiltro, setUsuarioFiltro] = useState([]);
     const [page, setPage] = useState(1);
     const [quantity, setQuantity] = useState(1);
     const [quantityPerPage] = useState(7);
 
-    async function buscaRespostas(page, usuario){
-        
-        if(!localStorage.getItem(Config.TOKEN)){
+    async function buscaRespostas(page, usuario) {
+
+        if (!localStorage.getItem(Config.TOKEN)) {
             toast.info('Necessário logar para acessar!');
-            navigate('/', {replace: true});
+            navigate('/', { replace: true });
             return;
         }
         setLoadding(true);
 
         await api.get('/RespostasUsuaro/getHistory?page=' + page + '&quantity=' + quantityPerPage + '&userCode=' + usuario)
-        .then((response) => {
-            if(response.data.success){
-                setRespostas(response.data.object);
-                setQuantity(response.data.total);
-                setLoadding(false);
-            }
-            else{
-                navigate('/', {replace: true});
-                toast.warn('Erro ao buscar');    
-            }
-        })
-        .catch(() => {
-            navigate('/', {replace: true});
-            toast.warn('Erro ao buscar');
-        })
+            .then((response) => {
+                if (response.data.success) {
+                    setRespostas(response.data.object);
+                    setQuantity(response.data.total);
+                    setLoadding(false);
+                }
+                else {
+                    navigate('/', { replace: true });
+                    toast.warn('Erro ao buscar');
+                }
+            })
+            .catch(() => {
+                navigate('/', { replace: true });
+                toast.warn('Erro ao buscar');
+            })
     }
 
-    async function buscaUsuarios(){
-        if(!localStorage.getItem(Config.TOKEN)){
+    async function buscaUsuarios() {
+        if (!localStorage.getItem(Config.TOKEN)) {
             toast.info('Necessário logar para acessar!');
-            navigate('/', {replace: true});
+            navigate('/', { replace: true });
             return;
         }
 
         await api.get('/Usuarios')
-        .then((response) => {
-            if(response.data.success){
-                var prov = [];
-                response.data.object.forEach(element => {
-                    prov.push({
-                        value: element.id,
-                        label: element.nome
-                    })
-                });
-                setUsuarios(prov);
-                setQuantity(response.data.total);
-                setLoadding(false);
-            }
-            else{
-                navigate('/', {replace: true});
-                toast.warn('Erro ao buscar');    
-            }
-        })
-        .catch(() => {
-            navigate('/', {replace: true});
-            toast.warn('Erro ao buscar');
-        })
+            .then((response) => {
+                if (response.data.success) {
+                    var prov = [];
+                    response.data.object.forEach(element => {
+                        prov.push({
+                            value: element.id,
+                            label: element.nome
+                        })
+                    });
+                    setUsuarios(prov);
+                    setQuantity(response.data.total);
+                    setLoadding(false);
+                }
+                else {
+                    navigate('/', { replace: true });
+                    toast.warn('Erro ao buscar');
+                }
+            })
+            .catch(() => {
+                navigate('/', { replace: true });
+                toast.warn('Erro ao buscar');
+            })
     }
 
     useEffect(() => {
@@ -91,92 +91,111 @@ function Admin(){
         buscaRespostas(page, selectedOptions.value);
     }
 
-    function abreQuestao(codigoQuestao){
-        navigate('/questoes/codigoquestaohistorico:' + codigoQuestao, {replace: true});
+    function abreQuestao(codigoQuestao) {
+        navigate('/questoes/codigoquestaohistorico:' + codigoQuestao, { replace: true });
     }
 
     const handleChange = (event, value) => {
         setPage(value);
         setLoadding(true);
         buscaRespostas(value, usuarioFiltro.value);
-      };
+    };
 
-      function baixarBoletinDetalhado(){
+    function baixarBoletinDetalhado() {
         setLoadding(true);
         console.log(usuarioFiltro);
         api.get('/RespostasQuestoes/reportDetail?codigoUsuario=' + usuarioFiltro.value)
-        .then((response) => {
-            setLoadding(false);
-            if(response.data.success){
-                const link = document.createElement('a');
-                link.href = response.data.object;
-                link.download = 'Histórico - ' + localStorage.getItem(Config.Nome) + '.html';
-                link.click();
-            }
-            else{
-                toast.error('Histórico não encontrado');
-            }
-        })
-        .catch((error) => {
-            setLoadding(false);
-            console.log(error);
-            toast.error('Erro ao gerar o boletinho!');
-        })
+            .then((response) => {
+                setLoadding(false);
+                if (response.data.success) {
+                    const link = document.createElement('a');
+                    link.href = response.data.object;
+                    link.download = 'Histórico - ' + localStorage.getItem(Config.Nome) + '.html';
+                    link.click();
+                }
+                else {
+                    toast.error('Histórico não encontrado');
+                }
+            })
+            .catch((error) => {
+                setLoadding(false);
+                console.log(error);
+                toast.error('Erro ao gerar o boletinho!');
+            })
     }
 
-    if(loadding){
-        return(
+    if (loadding) {
+        return (
             <div className='loaddingDiv'>
                 <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
             </div>
         )
     }
 
-    return(
+    return (
         <div className="containerpage">
-            <h1>Admin</h1>
-            <div className='descricao'>
-                <h3>Última respostas dos usuários:</h3>
-                <div className='formUsuario'>
-                    <div className='selectUsuario'>
-                        <p>Usuário:</p>
-                        <Select closeMenuOnSelect={false} components={animatedComponents} options={usuarios} onChange={handleUsuarios} style={{width: '100%'}}/>
+            <div className='dados global-infoPanel'>
+                <h1>Admin</h1>
+                <div className="separator separator--withMargins"></div>
+                <div className='descricao'>
+                    <h3>Última respostas dos usuários:</h3>
+                    <div className='formUsuario'>
+                        <div className='selectUsuario'>
+                            <p>Usuário:</p>
+                            <Select closeMenuOnSelect={false} components={animatedComponents} options={usuarios} onChange={handleUsuarios} style={{ width: '100%' }} />
+                        </div>
+                    </div>
+                    <div className='respostasUsuarios'>
+                        <p>
+                            Baixar histórico detalhado: <a target="_blank" onClick={() => baixarBoletinDetalhado()}>📩</a>
+                        </p>
+                        <h3>
+                            Histórico de questões respondidas por {usuarioFiltro.label}:
+                        </h3>
+                        {
+                            respostas?.map((item, index) => {
+                                return (
+                                    <div key={index} >
+                                        <h4 className='dadosResumidos'>
+                                            <p>
+                                                Prova: {item.nomeProva}📚
+                                            </p>
+
+                                            <p>
+                                                Questão: {item.numeroQuestao}
+                                            </p>
+                                            <p>
+                                                Resposta: {item.respostaCorreta === '1' ? "CORRETA🥳" : "INCORRETA😒"}
+                                            </p>
+                                            <p>
+                                                Data resposta: {item.dataResposta?.replace('T', ' ')}
+                                            </p>
+                                            <p>
+                                                <button className='global-button global-button--full-width' onClick={() => abreQuestao(item.codigoQuestao)}>Visualizar Questão✏️</button>
+                                            </p>
+                                        </h4>
+                                    </div>
+                                )
+                            })
+                        }
+
                     </div>
                 </div>
-                <div className='respostasUsuarios'>
-                    <p>
-                        Baixar histórico detalhado: <a target="_blank" onClick={() => baixarBoletinDetalhado()}>📩</a>
-                    </p>
-                    <h3>
-                        Histórico de questões respondidas por {usuarioFiltro.label}:
-                    </h3>
-                {   
-                    respostas?.map((item, index) => {
-                        return(
-                            <div key={index} >
-                                <h4>
-                                    Prova: {item.nomeProva}📚
-                                    <br/>
-                                    Questão: {item.numeroQuestao}
-                                    <br/>
-                                    Resposta: {item.respostaCorreta === '1' ? "CORRETA🥳" : "INCORRETA😒"}
-                                    <br/>
-                                    Data resposta: {item.dataResposta?.replace('T', ' ')}
-                                    <br/>
-                                    <button className='global-button global-button--full-width' onClick={() => abreQuestao(item.codigoQuestao)}>Visualizar Questão✏️</button>
-                                    <br/>
-                                    <br/>
-                                </h4>
-                            </div>
-                        )
-                    })
-                }
+            </div>
+            <div className='itensPaginacao global-mt'>
                 {
                     <Stack spacing={4}>
-                        <Pagination count={parseInt((quantity/quantityPerPage)+1)} page={page} color="primary" showFirstButton showLastButton onChange={handleChange}/>
-                    </Stack>    
+                        <Pagination sx={{
+                            '& .Mui-selected': {
+                                color: 'white'
+                            },
+                            '& .MuiPaginationItem-root': {
+                                color: 'white',
+
+                            }
+                        }} count={parseInt((quantity / quantityPerPage) + 1)} page={page} color="primary" showFirstButton showLastButton onChange={handleChange} />
+                    </Stack>
                 }
-                </div>
             </div>
         </div>
     )
