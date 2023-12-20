0510580ae5f117af4f7e2d925d5a43f53fd73002
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2023-12-20 11:20:39 -0300

    Validando quando usuário perde a sessão.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import Config from './../config.json';
+import { toast } from 'react-toastify';
 
 const api = axios.create({
-    //baseURL: 'https://localhost:7119/api',
-    baseURL: 'https://apisunsale.azurewebsites.net/api',
+    baseURL: 'https://localhost:7119/api',
+    //baseURL: 'https://apisunsale.azurewebsites.net/api',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -14,5 +15,24 @@ const token = localStorage.getItem(Config.TOKEN);
 if (token) {
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
+
+// Add an interceptor to handle unauthorized responses
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.setItem(Config.LOGADO, 0);
+        localStorage.setItem(Config.USUARIO, '');
+        localStorage.setItem(Config.TOKEN, '');
+        localStorage.setItem(Config.ADMIN, '');
+        localStorage.setItem(Config.TEMPO_PARAM, 0);
+        localStorage.removeItem(Config.LOGADO);
+        localStorage.removeItem(Config.USUARIO);
+        localStorage.removeItem(Config.TOKEN);
+        localStorage.removeItem(Config.ADMIN);
+      }
+      return Promise.reject(error);
+  }
+);
 
 export default api;
